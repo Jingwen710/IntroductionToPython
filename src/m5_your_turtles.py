cd933d5f720717fda88a01a249ffdf76721d8a69
wuj1 <wuj1@rose-hulman.edu>
2017-11-27 00:54:53 -0500

    I created two turtles and made them draw pictures.

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Jingwen Wu.
 """
 ########################################################################
-# TODO: 1.
+# Done: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# Done: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,43 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+Marvel = rg.SimpleTurtle('turtle')
+Marvel.pen = rg.Pen('Blue', 3)
+Marvel.speed = 5
+
+size = 100
+
+for k in range(10):
+    Marvel.draw_circle(size)
+    Marvel.pen_up()
+    Marvel.right(60)
+    Marvel.forward(10)
+    Marvel.pen_down()
+    size = size - 10
+
+DC = rg.SimpleTurtle('turtle')
+DC.pen = rg.Pen('Red', 4)
+DC.speed = 10
+DC.pen_up()
+DC.backward(200)
+
+length = 100
+
+for k in range(12):
+    DC.draw_regular_polygon(5,length)
+    DC.pen_up()
+    DC.right(90)
+    DC.forward(10)
+    DC.left(90)
+    DC.pen_down()
+    length = length - 8
+
+
+
+window.close_on_mouse_click()
+
